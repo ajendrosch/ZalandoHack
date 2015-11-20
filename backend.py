a431f94eb105abfbf9be1950bee47b1dc45bbce2
Sebastian Muehr <sebastian.muehr@gmail.com>
2015-11-20 03:48:44 +0100

    some update

--- backend.py
+++ backend.py
@@ -1,20 +1,26 @@
 from flask import Flask, request
-from flask.ext.cors import CORS
 
 import json
+import base64
 
-UPLOAD_FOLDER = '/uploads'
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-CORS(app)
 
 @app.route("/upload", methods=['POST'])
 def upload():
     img  = request.form['img']
+    img = img.split(",")[1]
+    fh = open("imageToSave.png", "wb")
+    fh.write(img.decode('base64'))
+    fh.close()
     data = {}
     data["result"] = "test"
     return json.dumps(data)
 
 if __name__ == "__main__":
     app.run(port=80)
+
+
+
+
+    
