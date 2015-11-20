d613bc60cf6c1bc89a9d85421f354ae04cd68da3
Sebastian Muehr <sebastian.muehr@gmail.com>
2015-11-20 01:55:00 +0100

    backend stuff

--- backend.py
+++ backend.py
@@ -3,12 +3,16 @@ from flask.ext.cors import CORS
 
 import json
 
+UPLOAD_FOLDER = '/uploads'
+
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/upload", methods=['POST'])
 def upload():
-    return request.form['img']
+    file = request.files['img']
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+    return os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
 
 if __name__ == "__main__":
     app.run(port=80)
